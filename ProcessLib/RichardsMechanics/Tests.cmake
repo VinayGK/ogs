c37563de6d2b53b7958388a27319377f4501a384
Vinay <vinaygkumar@icloud.com>
2026-03-18 14:52:03 +0100

    RM: add BEACON run-only VK smoke cases

--- ProcessLib/RichardsMechanics/Tests.cmake
+++ ProcessLib/RichardsMechanics/Tests.cmake
@@ -83,6 +83,36 @@ AddTest(
 )
 
 AddTest(
+    NAME RichardsMechanics_beacon_1a01_vk_smoke
+    PATH RichardsMechanics
+    EXECUTABLE ogs
+    RUNTIME 20
+    EXECUTABLE_ARGS beacon_1a01_vk_smoke.xml
+    WRAPPER time
+    REQUIREMENTS NOT OGS_USE_MPI
+)
+
+AddTest(
+    NAME RichardsMechanics_beacon_1b_vk_smoke
+    PATH RichardsMechanics
+    EXECUTABLE ogs
+    RUNTIME 20
+    EXECUTABLE_ARGS beacon_1b_vk_smoke.xml
+    WRAPPER time
+    REQUIREMENTS NOT OGS_USE_MPI
+)
+
+AddTest(
+    NAME RichardsMechanics_beacon_1c_vk_smoke
+    PATH RichardsMechanics
+    EXECUTABLE ogs
+    RUNTIME 20
+    EXECUTABLE_ARGS beacon_1c_vk_smoke.xml
+    WRAPPER time
+    REQUIREMENTS NOT OGS_USE_MPI
+)
+
+AddTest(
     NAME RichardsMechanics_square_1e2_confined_compression_restart
     PATH RichardsMechanics
     EXECUTABLE ogs
