30cc58cced97b81230e804eb6ed987d76c0415ed
Vinay <vinaygkumar@icloud.com>
2026-03-18 16:18:20 +0100

    RM: harden BEACON 1a01 as VK reference regression

--- ProcessLib/RichardsMechanics/Tests.cmake
+++ ProcessLib/RichardsMechanics/Tests.cmake
@@ -93,6 +93,24 @@ AddTest(
 )
 
 AddTest(
+    NAME RichardsMechanics_beacon_1a01_vk_reference
+    PATH RichardsMechanics
+    EXECUTABLE ogs
+    RUNTIME 20
+    EXECUTABLE_ARGS beacon_1a01_vk_smoke.prj
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT OGS_USE_MPI
+    DIFF_DATA
+    beacon_1a01_reference_t_1000.000000.vtu beacon_1a01_vk_smoke_t_1000.000000.vtu displacement displacement 1e-16 0
+    beacon_1a01_reference_t_1000.000000.vtu beacon_1a01_vk_smoke_t_1000.000000.vtu pressure pressure 1e-16 1e-12
+    beacon_1a01_reference_t_1000.000000.vtu beacon_1a01_vk_smoke_t_1000.000000.vtu saturation saturation 1e-14 0
+    beacon_1a01_reference_t_1000.000000.vtu beacon_1a01_vk_smoke_t_1000.000000.vtu micro_pressure micro_pressure 1e-16 1e-12
+    beacon_1a01_reference_t_1000.000000.vtu beacon_1a01_vk_smoke_t_1000.000000.vtu micro_saturation micro_saturation 1e-14 0
+    beacon_1a01_reference_t_1000.000000.vtu beacon_1a01_vk_smoke_t_1000.000000.vtu swelling_stress swelling_stress 1e-16 0
+)
+
+AddTest(
     NAME RichardsMechanics_beacon_1b_vk_smoke
     PATH RichardsMechanics
     EXECUTABLE ogs
